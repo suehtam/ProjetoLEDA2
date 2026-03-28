1205e41c4613552d0975c8bd45463f95d1f619be
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2026-03-28 17:39:05 -0300

    Add Produto class with properties and comparison

---
+++ Produto.java
@@ -0,0 +1,32 @@
+public class Produto implements Comparable<Produto> {
+    private int id;
+    private String nome;
+    private int quantidade;
+    private double preco;
+
+    public Produto(int id, String nome, int quantidade, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco; }
+
+    // O critério de comparação padrão (usado pela AVLTree) será o ID
+    @Override
+    public int compareTo(Produto outro) {
+        return Integer.compare(this.id, outro.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "id=" + id + ", nome='" + nome + '\'' + ", qtd=" + quantidade + '}';
+    }
+}
